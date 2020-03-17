68f2b7db41314021c6304aac9151c92057e5fbec
ApacheAlpha <1706670065@qq.com>
2020-03-17 13:32:02 +0800

    git操作

---
+++ git/git.js
@@ -0,0 +1,17 @@
+//显示代码库状态
+git status
+//clone代码到本地
+git clone 项目地址
+//创建分支
+git branch 分支名
+// 创建分支并切换到该分支
+git checkout -b 分支名
+//往分支提交代码
+git commit -m '提交备注'
+//push代码到分支
+git push origin 分支名
+//对比当前状态和版本库中状态的变化
+git diff
+
+
+
