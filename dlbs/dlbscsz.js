b98c6df5b0875da7609e61f4325294d7ad03c32f
ApacheAlpha <2967902214@qq.com>
2020-03-20 12:56:59 +0800

    解决代码错误写法以及优化

---
+++ dlbs/dlbscsz.js
@@ -0,0 +1,52 @@
+// 可以多例部署的猜数字游戏，
+// 之前代码风格只生效了部分，已经重新配置
+const Koa = require('koa')
+const redis = require('redis')
+const _ = require('lodash')
+const Router = require('koa-router')
+const cors = require('koa-cors')
+
+const koans = new Koa()
+const routers = new Router()
+
+
+// 建立redis的链接
+const client = redis.createClient(6379, '127.0.0.1')
+
+routers.get('/start', (ctx) => {
+	const num = _.random(0, 100)
+	// 每次start开始就重置并保存num的进入redis
+	client.set('num', num)
+	ctx.body = 'OK'
+})
+
+// 获取num,目前就会这种写法
+function getnum() {
+	return new Promise((resolve, reject) => {
+		client.get('num', (err, data) => {
+			if (err) {
+				reject(err)
+			} else {
+				resolve(data)
+			}
+		})
+	})
+}
+
+routers.get('/:number', async (ctx) => {
+	const GetNum = Number(ctx.params.number)
+	// 用data接收await getnum()的值，减少await getnum()调用
+	const data = await getnum()
+	if (data > GetNum) {
+		ctx.body = 'smaller'
+	} else if (data < GetNum) {
+		ctx.body = 'bigger'
+	} else {
+		ctx.body = 'equal'
+	}
+})
+
+koans.use(cors())
+koans.use(routers.routes())// 作用：启动路由
+koans.use(routers.allowedMethods())
+koans.listen(3000)
