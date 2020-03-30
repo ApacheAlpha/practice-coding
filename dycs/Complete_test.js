bec5da8d6a55e350da6ac5bc630109f29f5aab68
ApacheAlpha <1706670065@qq.com>
2020-03-30 12:39:53 +0800

    修改错误测试写法

--- dycs/Complete_test.js
+++ dycs/Complete_test.js
@@ -1,59 +1,49 @@
 const _ = require('lodash')
-const req = require('request')
+const req = require('request-promise')
+const should = require('should')
 
-function start() {
-	return new Promise((resolve) => {
-		req.get('http://127.0.0.1:3001/start', (err, response, body) => {
-			if (!err && response.statusCode === 200) {
-				body.should.eql('OK')
-				resolve(body)
-			}
-		})
-	})
+async function start() {
+		const body = await req.get('http://127.0.0.1:3000/start')
+		console.log(body)
+		return body
 }
 
-
-function compareNumbers(data, min, max) {
-	return new Promise((resolve) => {
-		req.get(`http://127.0.0.1:3000/${data}`, (err, response, body) => {
-			if (!err && response.statusCode === 200) {
-				if (body === 'smaller') {
-					const nums = _.ceil((data + max) / 2)
-					return compareNumbers(nums, data, max)
-				} if (body === 'bigger') {
-					const nums = _.ceil((data + min) / 2)
-					return compareNumbers(nums, min, data)
-				} if (body === 'equal') {
-					resolve(data)
-				}
-			}
-		})
-	})
+async function compareNumbers(data, min, max) {
+		const body = await req.get(`http://127.0.0.1:3000/${data}`)
+		if (body === 'smaller') {
+				const nums = _.ceil((data + max) / 2)
+				return compareNumbers(nums, data, max)
+		}
+		if (body === 'bigger') {
+				const nums = _.ceil((data + min) / 2)
+				return compareNumbers(nums, min, data)
+		}
+		if (body === 'equal') {
+				return body
+		}
 }
 
 // 第一种写法
 describe('GET /number', () => {
-	it('should equal ', () => {
-		const MIN = 0
-		const MAX = 100000
-		const num = _.random(0, MAX)
-		start().then(() => {
-			return compareNumbers(num, MIN, MAX).then((data) => {
-				data.should.equal('equal')
-			})
+		it('should equal ', () => {
+				const MIN = 0
+				const MAX = 100000
+				const num = _.random(0, MAX)
+				return start().then(() => {
+						compareNumbers(num, MIN, MAX).then((data) => data.should.equal('equal'))
+				})
 		})
-	})
 })
 
 // 第二种写法
 describe('GET /number', () => {
-	it('should equal ', () => {
-		const MIN = 0
-		const MAX = 100000
-		const num = _.random(0, MAX)
-		start().then(async () => {
-			const data = await compareNumbers(num, MIN, MAX)
-			data.should.equal('equal')
+		it('should equal ', async () => {
+				const MIN = 0
+				const MAX = 100000
+				const num = _.random(0, MAX)
+				const data = await start()
+				data.should.equal('OK')
+				const number = await compareNumbers(num, MIN, MAX)
+				number.should.equal('equal')
 		})
-	})
 })
